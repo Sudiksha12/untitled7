fddbfdca8cbf81188ecd1c0ccf7e0a7e71c824bd
Sudiksha12 <sudiksha.kaushik_cs20@gla.ac.in>
2022-01-23 18:59:29 +0530

    ...

--- src/EFArrayDemo.java
+++ src/EFArrayDemo.java
@@ -7,12 +7,11 @@ public class EFArrayDemo {
         int arr[]; //declaration
         arr = new int[5]; //creation
         Scanner s = new Scanner(System.in);
-        for (int i = 0; i < arr.length ; i++) {
+        for (int i = 0; i < arr.length; i++) {
             arr[i] = s.nextInt();
         }
-        for (int rec: arr) {
+        for (int rec : arr) {
             System.out.println(rec);
-
         }
     }
 }
