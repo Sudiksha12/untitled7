f8c4a4f3b7dd0b2b2c5ac8b6932f0d24a1345630
Sudiksha12 <sudiksha.kaushik_cs20@gla.ac.in>
2022-01-23 18:31:18 +0530

    ...

---
+++ src/EFArrayDemo.java
@@ -0,0 +1,18 @@
+// create an array of 5 elements and print the values.
+
+import java.util.Scanner;
+
+public class EFArrayDemo {
+    public static void main(String[] args) {
+        int arr[]; //declaration
+        arr = new int[5]; //creation
+        Scanner s = new Scanner(System.in);
+        for (int i = 0; i < arr.length ; i++) {
+            arr[i] = s.nextInt();
+        }
+        for (int rec: arr) {
+            System.out.println(rec);
+
+        }
+    }
+}
